bd701083b145eff5b93455fa4a04d5e395546cb5
anton <anton@DESKTOP-H3DC1V2>
2024-11-04 16:43:51 +0300

    Добавьте файлы проекта.

---
+++ Веб-приложение-Управление-Данными/Program.cs
@@ -0,0 +1,15 @@
+namespace Веб_приложение_Управление_Данными
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
